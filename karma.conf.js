7e5e13a0f02a096110d3a1347194727644f5fbd0
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-30 22:29:42 +0530

    final

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Cricket-Team-Selection'),
+      dir: require('path').join(__dirname, './coverage/FirstAngularApp'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
